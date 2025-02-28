da8c68ed7821f22677b84f29264e3ce13bbc5031
Accany1 <129660499+Accany1@users.noreply.github.com>
2025-02-28 15:05:49 +0800

    Added working quantity buttons to cart

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,13 +1,50 @@
 import styles from '../styles/CartItem.module.css'
+import { useOutletContext } from 'react-router-dom'
 
-const CartItem = ({name, image, price, quantity}) => {
+const CartItem = ({name, image, price, quantity, id}) => {
+    const {selectedList} = useOutletContext()
+    const [selected, setSelected] = selectedList
+
+    const handleAddQuantity = (id) => {
+        let newSelected = selected.map(p => {
+            if (p.id === id) {
+                return {...p, quantity: p.quantity + 1}
+            } else {
+                return p
+            }
+        })
+        setSelected([...newSelected])
+    }
+
+    const handleReduceQuantity = (id) => {
+        if (selected.filter(p => p.id === id)[0].quantity === 0) {
+            setSelected(selected.filter(p => p.id !== id))
+            return
+        }
+        
+        let newSelected = selected.map(p => {
+            if (p.id === id) {
+                return {...p, quantity: p.quantity - 1}
+            } else {
+                return p
+            }
+        })
+        setSelected([...newSelected])
+    }
+
+
     return (
         <div className={styles.cartItem}>
             <img className={styles.image} src={image} alt={name} />
             <div className={styles.name}>{name}</div>
             <div className={styles.groupContainer}>
                 <div className={styles.subHeading}>Quantity</div>
-                <div>{quantity}</div>
+                <div className={styles.quantityContainer}>
+                    <button className={styles.button} onClick={() => handleAddQuantity(id)}>+</button>
+                    <div>{quantity}</div>
+                    <button className={styles.button} onClick={() => handleReduceQuantity(id)}>-</button>
+                </div>
+                
             </div>
             <div className={styles.groupContainer}>
                 <div className={styles.subHeading}>Price</div>
